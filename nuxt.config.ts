a5d09a8a2ed4091cbf982570f98b4f99bcd79630
Dzmitry_Shaviaka <krayzerkyn@gmail.com>
2025-02-07 13:47:31 +0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+		// head: {
+		// 	link: [
+		// 		{
+		// 			href: 'https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap',
+		// 			rel: 'stylesheet',
+		// 		},
+		// 		{ rel: 'icon', type: 'image/x-icon', href: '/CinemaGuide/favicon.png' },
+		// 	],
+		// },
+		baseURL: '/telegram-mini-app/',
+	},
+
+	nitro: {
+		output: {
+			dir: './dist/telegram-mini-app',
+		},
+	},
 })
