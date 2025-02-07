6f44b6bc109482ce742dabf75fd1df14f96304a1
Dzmitry_Shaviaka <krayzerkyn@gmail.com>
2025-02-07 18:03:02 +0300

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
    <section>
-    <h1>Hello Telegram</h1>
+      <button aria-haspopup="dialog" @click="open">
+          Open popup
+      </button>
    </section>
 </template>
 <script lang="ts" setup>
+import { popup } from '@telegram-apps/sdk-vue'
+
+const props = defineProps<{ title: string, message: string }>()
+
+function open() {
+  if (popup.isSupported()) {
+    popup.open(props);
+    return;
+  }
+}
+
 
 </script>
 <style lang="" scoped>
